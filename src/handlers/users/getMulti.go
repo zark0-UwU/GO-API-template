a806b5543b3e1ddb9f2095bc834ead8d818e6ffc
zark0 <58535242+zark0-UwU@users.noreply.github.com>
2022-05-07 00:44:54 +0200

    🚸 Tweaks to the users endpoint

--- src/handlers/users/getMulti.go
+++ src/handlers/users/getMulti.go
@@ -2,6 +2,7 @@ package users
 
 import (
 	"GO-API-template/src/config"
+	"GO-API-template/src/helpers/stdMessages"
 	"GO-API-template/src/models"
 	"context"
 	"fmt"
@@ -9,13 +10,15 @@ import (
 
 	"github.com/gofiber/fiber/v2"
 	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 type rangeUser struct {
 	Status  string      `json:"status"`
 	Message string      `json:"message"`
-	Amount  int         `json:"amount"`
+	Total   int64       `json:"total"`
+	Count   int         `json:"count"`
 	Offset  int         `json:"offset"`
 	Limmit  int         `json:"limmit"`
 	Next    string      `json:"next"`
@@ -36,7 +39,7 @@ type rangeUser struct {
 func GetUsers(c *fiber.Ctx) error {
 	offset, offsetErr := strconv.Atoi(c.Query("o", "0"))
 	limmit, limmitErr := strconv.Atoi(c.Query("l", "10"))
-	if (offset - limmit) > 100 {
+	if (limmit - offset) > 100 {
 		limmit = offset + 100
 	}
 	if offsetErr != nil || limmitErr != nil {
@@ -48,29 +51,40 @@ func GetUsers(c *fiber.Ctx) error {
 		})
 	}
 
-	projection := bson.M{"_id": 1, "username": 1, "role": 1}
+	projection := bson.M{"_id": 1, "username": 1, "role": 1} // to match models.user.minimal
 	cursor, err := models.UsersCollection.Find(
 		context.Background(),
 		bson.D{},
 		options.Find().SetSkip(int64(offset)).SetLimit(int64(limmit)).SetProjection(projection))
-	if err != nil {
-
+	if err != nil && err != mongo.ErrNoDocuments {
+		c.Status(fiber.StatusInternalServerError).JSON(
+			stdMessages.ErrorDefault("An error ocurred while retrieving the users data", nil),
+		)
 	}
 
 	var users []models.UserMinimal
-	cursor.All(context.Background(), &users)
+	err = cursor.All(context.Background(), &users)
+	if err != nil && err != mongo.ErrNoDocuments {
+		c.Status(fiber.StatusInternalServerError).JSON(
+			stdMessages.ErrorDefault("An error ocurred while retrieving the users data", nil),
+		)
+	}
 
 	r := limmit - offset
-	next := ""
+	var next string
 	if r <= len(users) {
+		fmt.Println(c.Request().URI())
 		next = c.BaseURL() + config.BasePath + fmt.Sprintf("/users?o=%v&l=%v", offset+r, limmit+r)
 	}
+
+	total, err := models.UsersCollection.CountDocuments(context.Background(), bson.M{})
 
 	return c.Status(fiber.StatusOK).JSON(rangeUser{
 		Status:  "success",
 		Offset:  offset,
 		Limmit:  limmit,
-		Amount:  len(users),
+		Total:   total,
+		Count:   len(users),
 		Next:    next,
 		Message: "Sucessfuly found users",
 		Users:   users,
